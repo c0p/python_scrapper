293bbe7f1407a522f54feea25070802441a2ba43
Jerald Cascayan <casourcer@gmail.com>
2018-01-19 12:08:38 -1000

    code change
    python2.x

--- web.py
+++ web.py
@@ -1,9 +1,68 @@
-import urllib2
+import urllib
+
+
+
+
+
+class WebScraper(object):
+
+
+  def __init__(self, url):
+    #checks and verifys
+    self.url = url
+    f = urllib.urlopen(url)
+    print ("pinging to " + self.url)
+
+    try:
+      f = urllib.urlopen(url)
+    except urllib.error.HTTPError as e:
+       print('HTTPError: '+e.code)
+       print('invalid HTTP')
+    except urllib.error.URLError as e:
+     print('URLError: '+e.reason)
+     print('invalid URL')
+    else:
+
+      print('Connection Success! What do you wanna do?')
+      print('[ a ] : Display HTML Contents')
+      print('[ b ] : Display Data Contents')
+      print('[ c ] : Display HTML Contents')
+      userOption = raw_input("Choose Option >  ")
+
+      if userOption == "a":
+        self.viewHTMLContents(f)
+      elif userOption =="b":
+        self.Web_Headers(f)
+
+
+  def viewHTMLContents(self, url):
+    #print HTML contents
+    print url.read()
+
+  def Web_Headers(self, url):
+    print "     "
+    print 'RESPONSE:', url
+    print 'URL     :', url.geturl()
+
+    headers = url.info()
+    print 'DATE    :', headers['date']
+    print 'HEADERS :'
+    print '----------'
+    print headers
+
+    data = url.read()
+    print 'DATA LENGTH  :', len(data)
+
+
+
+
+
+###random bluaghwqeqwe
 try:
-    urllib2.urlopen('http://www.google.com')
-    html = response.read()
-    print(html)
-except urllib2.HTTPError, e:
-    print(e.code)
-except urllib2.URLError, e:
-    print(e.args)
+    while True:
+      websiteInput = raw_input("Enter Desired URL > ")
+      User_URL = "https://" + websiteInput
+      demo = WebScraper(User_URL)
+except KeyboardInterrupt:
+    pass
+
